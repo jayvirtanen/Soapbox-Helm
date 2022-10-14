22cdc2cb1232add951928416238ca31255550827
Jay Virtanen <83300606+jayvirtanen@users.noreply.github.com>
2022-10-14 15:53:25 -0400

    Update prod.secret.exs

--- prod.secret.exs
+++ prod.secret.exs
@@ -8,9 +8,9 @@ import Config
 config :pleroma, Pleroma.Web.Endpoint,
    url: [host: "INSTANCE_NAME", scheme: "https", port: 443],
    http: [ip: {0, 0, 0, 0}, port: 4000],
-   secret_key_base: "jrqlt38x80Wt596CEi+ggwRBvgeoueQGWukrAWPG5MUZzigNS8uQgQhdclj3f36Z",
-   live_view: [signing_salt: "7sgvCLIn"],
-   signing_salt: "JUssJFa0"
+   secret_key_base: "SECRETKEYBASE",
+   live_view: [signing_salt: "SIGNSALT"],
+   signing_salt: "SIGNSALT"
 
 config :pleroma, :instance,
   name: "INSTANCE_NAME",
@@ -33,13 +33,13 @@ config :pleroma, Pleroma.Repo,
 # Configure web push notifications
 config :web_push_encryption, :vapid_details,
   subject: "mailto: ADMIN_EMAIL",
-  public_key: "BNC1h1ziIdfyWI9w2Ja9OAmTi4nkZTMNCwhaZIXprs1meU6olaswiLaL8XkjJ_CbVWK8R5ZOHp9XoC3AyP1CJ8w",
-  private_key: "3ZpSo7s49__1S92Z2JLIGOdGR64-k6p1aFGu1c4oI8A"
+  public_key: "PUBLICKEY",
+  private_key: "PRIVATEKEY"
 
 config :pleroma, :database, rum_enabled: false
 config :pleroma, :instance, static_dir: "/var/lib/pleroma/static"
 config :pleroma, Pleroma.Uploaders.Local, uploads: "/var/lib/pleroma/uploads"
 
-config :joken, default_signer: "PD1drGp6NaEtJZNdsyev49xcm3Qi08a8AAqJwjeYaBO7REr5LtUrbkOBwwR8se65"
+config :joken, default_signer: "DEFAULTSIGNER"
 
 config :pleroma, configurable_from_database: true
