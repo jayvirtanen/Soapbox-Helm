b8c0140cbfef4f152d146ab3ddc7a71698290722
Jay Virtanen <83300606+jayvirtanen@users.noreply.github.com>
2022-10-14 15:34:59 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sleep 6h
+set -e
 
+echo "-- Waiting for database..."
+while ! pg_isready -U pleroma -d postgres://${PLEROMA_POSTGRESQL_SERVICE_HOST}:5432/pleroma -t 1; do
+    sleep 1s
+done
+
+echo "-- Running migrations..."
+$HOME/bin/pleroma_ctl migrate
+
+echo "-- Starting!"
+exec $HOME/bin/pleroma start
