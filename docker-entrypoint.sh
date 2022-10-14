82c61e72de82374afcf0e85bc21791be954bc39e
Jay Virtanen <83300606+jayvirtanen@users.noreply.github.com>
2022-10-14 14:47:08 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "-- Waiting for database..."
-while ! pg_isready -U ${DB_USER} -d postgres://${PLEROMA_POSTGRESQL_SERVICE_HOST}:5432/pleroma -t 1; do
+while ! pg_isready -U pleroma -d postgres://${PLEROMA_POSTGRESQL_SERVICE_HOST}:5432/pleroma -t 1; do
     sleep 1s
 done
 
